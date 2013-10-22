de5c2792701e614e32592749a671fddd03b6c039
Daniel Freedman <dfreedm@google.com>
2013-10-22 16:30:48 -0700

    Use shared readManifest implementation in tools

--- packages/expressions/gruntfile.js
+++ packages/expressions/gruntfile.js
@@ -14,24 +14,7 @@
 
 module.exports = function(grunt) {
 
-  // recursive module builder
-  var path = require('path');
-  function readManifest(filename, modules) {
-    modules = modules || [];
-    var lines = grunt.file.readJSON(filename);
-    var dir = path.dirname(filename);
-    lines.forEach(function(line) {
-      var fullpath = path.join(dir, line);
-      if (line.slice(-5) == '.json') {
-        // recurse
-        readManifest(fullpath, modules);
-      } else {
-        modules.push(fullpath);
-      }
-    });
-    return modules;
-  }
-
+  var readManifest = require('../tools/loader/readManifest.js');
   grunt.initConfig({
     karma: {
       options: {
