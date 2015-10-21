b77c9f0ab9ebc9be27ad23f8e927e1e047cd0809
Justin Fagnani <justin@fagnani.com>
2015-10-21 13:29:48 -0700

    Add non-minified build, remove dead code elimination.

--- packages/expressions/gulpfile.js
+++ packages/expressions/gulpfile.js
@@ -1,37 +1,47 @@
+'use strict';
+
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 
-gulp.task('default', function () {
-  return gulp.src(['src/parser.js', 'src/eval.js'])
-      .pipe(babel({
-        modules: 'amd',
-        moduleRoot: 'polymer-expressions',
-        sourceRoot: 'src',
-        moduleIds: true,
-        loose: [
-          'es6.classes',
-          'es6.destructuring',
-          'es6.forOf',
-          'es6.modules',
-          'es6.properties.computed',
-          'es6.spread',
-          'es6.templateLiterals',
-        ],
-        optional: [
-          'minification.constantFolding',
-          'minification.deadCodeElimination',
-          'minification.memberExpressionLiterals',
-          'minification.propertyLiterals',
-        ],
-      }))
-      .pipe(concat('polymer-expressions.min.js'))
-      .pipe(uglify({
-        compress: true,
-        minify: {
-          sort: true,
-        },
-      }))
-      .pipe(gulp.dest('.'));
-});
+gulp.task('default', ['minify']);
+
+gulp.task('js', () => js()
+    .pipe(concat('polymer-expressions.js'))
+    .pipe(gulp.dest('.')));
+
+gulp.task('minify', () => js()
+    .pipe(concat('polymer-expressions.min.js'))
+    .pipe(uglify({
+      compress: true,
+      minify: {
+        sort: true,
+      },
+    }))
+    .pipe(gulp.dest('.')));
+
+let js = () => gulp.src(['src/parser.js', 'src/eval.js'])
+    .pipe(babel({
+      modules: 'amd',
+      moduleRoot: 'polymer-expressions',
+      sourceRoot: 'src',
+      moduleIds: true,
+      experimental: true,
+      loose: [
+        'es6.classes',
+        'es6.destructuring',
+        'es6.forOf',
+        'es6.modules',
+        'es6.properties.computed',
+        'es6.spread',
+        'es6.templateLiterals',
+      ],
+      optional: [
+        'minification.constantFolding',
+        // this is causing an error in babel :(
+        // 'minification.deadCodeElimination',
+        'minification.memberExpressionLiterals',
+        'minification.propertyLiterals',
+      ],
+    }));
