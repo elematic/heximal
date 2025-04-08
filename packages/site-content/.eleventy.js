5965e4e57cb1de4bd61e3fe9832f000c7d0c8bdb
Justin Fagnani <justin@fagnani.com>
2025-04-07 23:22:47 -0700

    [site] Add initial site content

--- packages/site-content/.eleventy.js
+++ packages/site-content/.eleventy.js
@@ -1,7 +1,22 @@
+import * as fs from 'node:fs';
+import * as pathlib from 'node:path';
+import {fileURLToPath} from 'node:url';
 import eleventyNavigationPlugin from '@11ty/eleventy-navigation';
+import syntaxHighlight from '@11ty/eleventy-plugin-syntaxhighlight';
 
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(syntaxHighlight);
+
+  eleventyConfig.addPassthroughCopy('site/*.css');
+
+  const openPropsPath = fileURLToPath(import.meta.resolve('open-props/style'));
+  symlinkForce(openPropsPath, 'out/open-props/open-props.min.css');
+
+  symlinkForce(
+    fileURLToPath(import.meta.resolve('open-props/normalize')),
+    'out/open-props/normalize.min.css',
+  );
 
   return {
     dir: {
@@ -9,4 +24,22 @@ export default function (eleventyConfig) {
       output: 'out',
     },
   };
+}
+
+function symlinkForce(target, path) {
+  try {
+    // Delete existing symlinks so that if we use Eleventy's built-in watch mode
+    // (which doesn't clean output first), we won't get an error because the
+    // symlink already exists.
+    fs.unlinkSync(path);
+  } catch (e) {
+    if (e.code !== 'ENOENT') {
+      throw e;
+    }
+  }
+  // Create parent directories because Eleventy doesn't create the output
+  // directory before invoking the config function, and also in case we want a
+  // symlink in a child directory.
+  fs.mkdirSync(pathlib.dirname(path), {recursive: true});
+  fs.symlinkSync(target, path);
 }
